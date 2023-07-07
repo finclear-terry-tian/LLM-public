2ac187ef02eb70a12d81c83c81e8abf1bcbf68bc
finclear-terry-tian <138838336+finclear-terry-tian@users.noreply.github.com>
2023-07-07 13:40:31 +1000

    Delete llm_private directory

--- llm_private/pages/home.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def app():
-
-    # Configure page
-    page = 'home'
-    st.title(f'{st.session_state.app.pages[page].icon} {st.session_state.app.pages[page].title}')
-    st.header("Welcome to FinClear's llm! 👋")
